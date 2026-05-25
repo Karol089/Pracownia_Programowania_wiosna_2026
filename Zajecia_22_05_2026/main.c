ba9d4253c01a68d0bc1be0caf9b30f0efecf3755
Karol089 <karol.maminski1@gmail.com>
2026-05-25 11:12:19 +0200

    22-05-2026 & 25-05-2026 Listy jednokierunkowe
    Ostatnio edytowane: 25-05-2026

--- Zajecia_22_05_2026/main.c
+++ Zajecia_22_05_2026/main.c
@@ -4,11 +4,28 @@
 // Listy jednokierunkowe
 void tutorial(); // czesc prezentacyjna
 
-//Zadanie 1
+// stosowana struktura
 struct elementM {
     int x;
     struct elementM* next;
 };
+
+// Utils
+void printNoHead(struct elementM* myList){
+    struct elementM* temp = myList;
+    while(temp != NULL){
+        printf("%d\n", temp->x);
+        temp = temp->next;
+    }
+}
+
+void printHead(struct elementM* myHeadList){
+    struct elementM* temp = myHeadList->next;
+    while(temp != NULL){
+        printf("%d\n", temp->x);
+        temp = temp->next;
+    }
+}
 
 // Zadanie 3
 void printListWithoutHead(struct elementM* myList){
@@ -40,7 +57,49 @@ void printListWithHead(struct elementM* myListHead){
 }
 
 // Zadanie 5
-void printPos(){
+void printPos(struct elementM* myList){
+    struct elementM* temp = myList;
+    while(temp != NULL){
+        if(temp->x > 0){
+           printf("%d\n", temp->x);
+        }
+        temp = temp->next;
+    }
+    return;
+}
+
+// Zadanie 6
+void printOdd(struct elementM* myHeadList){
+    struct elementM* temp = myHeadList->next;
+    while(temp != NULL){
+        if(temp->x % 2 != 0){
+           printf("%d\n", temp->x);
+        }
+        temp = temp->next;
+    }
+    return;
+}
+
+// Zadanie 7
+struct elementM* createNoHead(){
+    struct elementM* myNoHead = malloc(sizeof(struct elementM));
+    return myNoHead;
+}
+
+// Zadanie 8
+struct elementM* createWithHead(){
+    struct elementM* myHead = malloc(sizeof(struct elementM));
+    myHead->next = NULL;
+    return myHead;
+}
+
+// Zadanie 9
+void addFirst(struct elementM** myList, int a){
+    struct elementM* newElem = malloc(sizeof(struct elementM));
+    newElem->x = a;
+    newElem->next = *myList;
+    *myList = newElem;
+    return;
 }
 
 
@@ -59,25 +118,25 @@ int main()
     struct elementM noHead1 = {4, &noHead2};
     myList = &noHead1;
     /*
-    struct elementM temp = x1;
-    while(&temp != NULL){
-        printf("%d\n", temp.x);
-        temp = *temp.next;
+    struct elementM* temp = myList;
+    while(temp != NULL){
+        printf("%d\n", temp->x);
+        temp = temp->next;
     }
     */
 
     // Zadanie 2
+    struct elementM* myHeadList = malloc(sizeof(struct elementM));
+    myHeadList->next = NULL;
+    struct elementM myHead1 = {-12, NULL};
+    struct elementM myHead2 = {5, &myHead1};
+    struct elementM myHead3 = {4, &myHead2};
+    myHeadList->next = &myHead3;
     /*
-    struct elementM myList;
-    myList.next = NULL;
-    struct elementM x1 = {-12, NULL};
-    struct elementM x2 = {5, &x1};
-    struct elementM x3 = {4, &x2};
-    myList.next = &x3;
-    struct elementM temp = *myList.next;
-    while(&temp != NULL){
-        printf("%d\n", temp.x);
-        temp = *temp.next;
+    struct elementM* temp = myHeadList->next;
+    while(temp != NULL){
+        printf("%d\n", temp->x);
+        temp = temp->next;
     }
     */
 
@@ -104,10 +163,41 @@ int main()
     printListWithHead(myList);
     */
 
+    // Zadanie 5 test
+    //printPos(myList); // deklaracja listy - linijka 64
+
+    // Zadanie 6 test
+    //printOdd(myList); // deklaracja listy - linijka 90
+
+    // Zadanie 7 test
+    /*
+    struct elementM* testNoHead = createNoHead();
+    struct elementM test7 = {12, NULL};
+    testNoHead = &test7;
+    printf("%d\n", testNoHead->x);
+    */
 
 
+    // Zadanie 8 test
+    /*
+    struct elementM* testWithHead = createWithHead();
+    struct elementM test7 = {12, NULL};
+    testWithHead->next = &test7;
+    printf("%d\n", testWithHead->next->x);
+    */
 
+    // Zadanie 9 test
+    /*
+    printf("Przed dodaniem elementu:\n");
+    printNoHead(myList);
+    printf("%p\n", myList);
 
+    addFirst(&myList, 18);
+
+    printf("\nPo dodaniu elementu:\n");
+    printNoHead(myList);
+    printf("%p\n", myList);
+    */
 
     return 0;
 }
